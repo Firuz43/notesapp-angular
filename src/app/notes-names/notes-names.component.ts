cb9cd17a817f10279392a080446c1e7d249a6e52
Firuz43 <firuz1199@mail.ru>
2022-11-17 18:37:14 +0400

    change

--- src/app/notes-names/notes-names.component.ts
+++ src/app/notes-names/notes-names.component.ts
@@ -11,27 +11,31 @@ export class NotesNamesComponent implements OnInit {
   constructor(private http: NotesService) { }
 
 
+  // Recieving data from db.json
   notes: any = []
+
+  // Keep tracking for search functionality
   searchText: string = '';
+
+  // Creating a property for tracking addMode
+  addMode: boolean = false;
 
 
   ngOnInit(): void {
     this.getData()
   }
 
-
-
-  addMode: boolean = false;
-
-
+  // Truning on the addMode
   addNote(): void {
     this.addMode = true
   }
 
+  // Turning off the addMode
   goBack(): void {
     this.addMode = false
   }
 
+  // Making request using HttpClient for getting data from dj.json
   getData() {
     this.http.fetchData().subscribe((res) => {
       this.notes = res
