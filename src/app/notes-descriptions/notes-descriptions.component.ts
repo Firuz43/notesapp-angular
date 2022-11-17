7e0acb8f0a4a5cf4587b011c34ce26452d2851d6
Firuz43 <firuz1199@mail.ru>
2022-11-17 18:51:04 +0400

    changes

--- src/app/notes-descriptions/notes-descriptions.component.ts
+++ src/app/notes-descriptions/notes-descriptions.component.ts
@@ -9,27 +9,37 @@ import { NotesService } from '../services/notes.service';
 })
 export class NotesDescriptionsComponent implements OnInit {
 
+  // Storing NotesService to notesService variable
   constructor(private noteService: NotesService) { }
 
   ngOnInit(): void {
     this.onGetData()
   }
-  //Getting addMode from notes-name (parent component)
+  // Getting addMode from notes-name (parent component)
   @Input() addMode: boolean = false
 
+  // Keep tracking updateMode
   updateMode: boolean = false;
 
+  // Making property and recieving id from onEdit() method
   currentNoteId: number | any;
+
+  // Creating notes property for storing data from server
   notes: any = []
+
+  // Making @Viewchild for for using form values in input forms to update
   @ViewChild('myForm') form: NgForm | any;
 
 
+  // Getting data from server (db.json) and storing it to our notes variable
   onGetData() {
     this.noteService.fetchData().subscribe((res) => {
       this.notes = res;
     })
   }
 
+  // notesCreate inside has updateProduct also if the updateMethod is false we are calling createNote() method
+  // else if updateMode is true we are calling updateProduct
   notesCreate(notes: { id: string, title: string, description: string, date: Date }) {
     if (!this.updateMode) {
       this.noteService.createNote(notes);
